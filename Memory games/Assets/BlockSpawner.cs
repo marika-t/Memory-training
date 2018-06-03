dc514031500c1856e366b4994df467958b80b91d
marika-t <trudnowska.m@gmail.com>
2018-06-03 21:03:03 +0200

    major update
    added warm up excerice, menu is now mostly functional

---
+++ Memory games/Assets/BlockSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour {
+
+	public Transform[] spawnPoints;
+	public GameObject blockPrefab;
+	private float timeToSpawn = 2f;
+	public float timeBetweenSpawns = 1f;
+
+	// Use this for initialization
+	void Update () 
+	{
+		if (Time.time >= timeToSpawn) {
+			SpawnBlocks ();
+			timeToSpawn = Time.time + timeBetweenSpawns;
+		}
+	}
+	void SpawnBlocks()
+	{
+		int rnd = Random.Range (0, spawnPoints.Length);
+
+		for (int i = 0; i < spawnPoints.Length; i++) {
+			if (rnd != i) {
+				Instantiate (blockPrefab, spawnPoints[i].position, Quaternion.identity);
+			}
+		}
+	}
+
+}
